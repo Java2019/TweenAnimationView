abd978c8b65ede2072d3dcd5316ba96cd4416848
java2019 <java_2019@mail.ru>
2017-07-10 10:54:40 +0300

    animation

--- app/src/main/java/com/samples/res/tweenanimationview/TweenAnimationActivity.java
+++ app/src/main/java/com/samples/res/tweenanimationview/TweenAnimationActivity.java
@@ -2,12 +2,27 @@ package com.samples.res.tweenanimationview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-public class TweenAnimationActivity extends AppCompatActivity {
+public class TweenAnimationActivity extends AppCompatActivity
+        implements View.OnClickListener{
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tween_animation);
+        imageView = (ImageView)findViewById(R.id.image);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Animation animation = AnimationUtils.loadAnimation(
+                getApplicationContext(), R.anim.anim_android);
+        imageView.startAnimation(animation);
     }
 }
